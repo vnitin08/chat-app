e79d1e2067a06584add5c04d070e314f194f5a3e
Nitin Verma <ch1221031@iitd.ac.in>
2024-03-29 00:30:47 +0530

    Integrated Socket.IO

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
 
 export const sendMessage = async(req, res) => {
     try {
@@ -26,13 +27,18 @@ export const sendMessage = async(req, res) => {
         if(newMessage){
             conversation.messages.push(newMessage);
         }
-
-        // Socket.io functionality to be added here
-
         // await conversation.save();
         // await newMessage.save();
-
         await Promise.all([conversation.save(), newMessage.save()]); // Save both conversation and message in parallel
+
+        // Socket.io functionality to be added here
+        const receiverSocketId = getReceiverSocketId(receiverId);
+        if (receiverSocketId) {
+            // io.to(<socketId>).emit() is used to send the events to a specific client
+            io.to(receiverSocketId).emit("newMessage", newMessage);
+        }
+
+        
         res.status(201).json(newMessage);
 
     } catch (error) {
